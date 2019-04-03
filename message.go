5498c4dbeebf50839f9dabd6d77d46055496ad0d
Crossle Song <crosslesong@gmail.com>
2019-04-04 00:23:33 +0800

    Fix compile

--- message.go
+++ message.go
@@ -24,7 +24,9 @@ func StartBlaze(db *sql.DB) error {
 	defer cancel()
 
 	for {
-		if err := bot.Loop(ctx, ResponseMessage{}, config.MixinClientId, config.MixinSessionId, config.MixinPrivateKey); err != nil {
+		blazeClient := bot.NewBlazeClient(config.MixinClientId, config.MixinSessionId, config.MixinPrivateKey)
+		channelMessage := ResponseMessage{blaze: blazeClient}
+		if err := blazeClient.Loop(ctx, channelMessage); err != nil {
 			session.Logger(ctx).Error(err)
 		}
 		session.Logger(ctx).Info("connection loop end")
@@ -40,9 +42,10 @@ func newBlazeContext(db *sql.DB, client *durable.LoggerClient) (context.Context,
 }
 
 type ResponseMessage struct {
+	blaze *bot.BlazeClient
 }
 
-func (r ResponseMessage) OnMessage(ctx context.Context, mc *bot.MessageContext, msg bot.MessageView, uid string) error {
+func (r ResponseMessage) OnMessage(ctx context.Context, msg bot.MessageView, uid string) error {
 	if msg.Category != bot.MessageCategorySystemAccountSnapshot && msg.Category != bot.MessageCategorySystemConversation && msg.ConversationId == bot.UniqueConversationId(config.MixinClientId, msg.UserId) {
 		if msg.Category == "PLAIN_TEXT" {
 			data, err := base64.StdEncoding.DecodeString(msg.Data)
@@ -52,28 +55,28 @@ func (r ResponseMessage) OnMessage(ctx context.Context, mc *bot.MessageContext, 
 			if "/start" == string(data) {
 				_, err = models.CreateSubscriber(ctx, msg.UserId)
 				if err == nil {
-					if err := bot.SendPlainText(ctx, mc, msg, "订阅成功"); err != nil {
+					if err := r.blaze.SendPlainText(ctx, msg, "订阅成功"); err != nil {
 						return bot.BlazeServerError(ctx, err)
 					}
 				}
 			} else if "/stop" == string(data) {
 				err = models.RemoveSubscriber(ctx, msg.UserId)
 				if err == nil {
-					if err := bot.SendPlainText(ctx, mc, msg, "已取消订阅"); err != nil {
+					if err := r.blaze.SendPlainText(ctx, msg, "已取消订阅"); err != nil {
 						return bot.BlazeServerError(ctx, err)
 					}
 				}
 			} else {
 				content := `发送 /start 订阅消息
 发送 /stop 取消订阅`
-				if err := bot.SendPlainText(ctx, mc, msg, content); err != nil {
+				if err := r.blaze.SendPlainText(ctx, msg, content); err != nil {
 					return bot.BlazeServerError(ctx, err)
 				}
 			}
 		} else {
 			content := `发送 /start 订阅消息
 发送 /stop 取消订阅`
-			if err := bot.SendPlainText(ctx, mc, msg, content); err != nil {
+			if err := r.blaze.SendPlainText(ctx, msg, content); err != nil {
 				return bot.BlazeServerError(ctx, err)
 			}
 		}
